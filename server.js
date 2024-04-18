a9d80e8cbc1d1c0b4f95dae461498427b5e8e7b9
Phoenix Staley <PhoenixStaley121@hotmail.com>
2024-04-18 16:19:42 -0700

    Add /getUser

--- server.js
+++ server.js
@@ -4,12 +4,36 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const database = [] // Stand in, while the AWS DB is being set up
 
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
     console.log("Request recieved");
     res.sendFile(__dirname + "/../public/index.html");
-})
+});
+
+app.get("/getUser", function (req, res) {
+    let found = false;
+
+    console.log(`Request for ${req.query.username} recieved`);
+
+    for (let i = 0; i < database.length; i++) {
+        if (database[i].username === req.query.username) {
+            found = true;
+            const user = database[i]
+            res.status(200).send({
+                username: user.username,
+                name: user.name,
+                isAdmin: user.isAdmin
+            });
+        }
+    }
+
+    if (!found) {
+        console.log("404 error");
+        res.status(404).send("Not found");
+    }
+});
 
 app.listen(PORT);
