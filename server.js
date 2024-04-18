92030084c7b1a3091dd7d380c7383b914fdfe971
Phoenix Staley <PhoenixStaley121@hotmail.com>
2024-04-18 15:11:25 -0700

    Add base path

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 // Created by Phoenix Staley
 // April 17, 2024
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    console.log("Request recieved");
+    res.sendFile(__dirname + "/../public/index.html");
+})
+
+app.listen(PORT);
