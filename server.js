f2b7171a614d0463d219c8b862be7b5b6e7bd994
Phoenix Staley <PhoenixStaley121@hotmail.com>
2024-04-23 11:57:42 -0700

    Add detailed server log

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.get("/", function (req, res) {
 app.get("/makeAdmin", function (req, res) {
     if (!req?.session?.profile?.username) {
         res.sendStatus(403);
+        console.log("/makeAdmin - 403 - Not logged in");
         return;
     }
 
@@ -49,6 +50,7 @@ app.get("/makeAdmin", function (req, res) {
             req.session.profile = user;
             database.users[i].isAdmin = true;
             res.sendStatus(200);
+            console.log(`/makeAdmin - 200 - ${req.session.profile.username}`);
             return;
         }
     }
@@ -57,6 +59,7 @@ app.get("/makeAdmin", function (req, res) {
 userRouter.post("/signUp", function (req, res) {
     if (!req.query.username || !req.query.name || !req.query.password_encoded) {
         res.status(400).send("No 'name', 'username', or 'password_encoded' query parameters.");
+        console.log("/signUp - 400 - Bad request");
         return;
     }
     
@@ -66,12 +69,11 @@ userRouter.post("/signUp", function (req, res) {
         bio: ""
     };
     const username = user.username;
-    
-    console.log(`Request to sign up ${username} recieved`);
 
     for (let i = 0; i < database.users.length; i++) {
         if (database.users[i].username === req.query.username) {
             res.status(400).send("Username taken");
+            console.log("/signUp - 400 - Username taken");
             return;
         }
     }
@@ -88,21 +90,18 @@ userRouter.post("/signUp", function (req, res) {
         isAdmin: user.isAdmin,
         bio: user.bio
     });
+    console.log(`/signUp - 201 - ${req.query.username}`);
 });
 
 userRouter.get("/getUser", function (req, res) {
     if (!req.query.username) {
         res.status(400).send("No 'username' query parameter");
+        console.log("/getUser - 400 - Bad request");
         return;
     }
-    
-    let found = false;
-
-    console.log(`Request for ${req.query.username} recieved`);
 
     for (let i = 0; i < database.users.length; i++) {
         if (database.users[i].username === req.query.username) {
-            found = true;
             const user = database.users[i]
             res.status(200).send({
                 username: user.username,
@@ -110,21 +109,21 @@ userRouter.get("/getUser", function (req, res) {
                 isAdmin: user.isAdmin,
                 bio: user.bio
             });
-            break;
+            console.log(`/getUser - 200 - ${user.username}`);
+            return;
         }
     }
 
-    if (!found) {
-        console.log("404 error");
-        res.status(404).send("Not found");
-    }
+    res.status(404).send("Not found");
+    console.log(`/getUser - 404 - ${req.query.username}`);
 });
 
 userRouter.put("/updateUser", function (req, res) {
-    if (Object.keys(req.query).length < 2) {
+    if (Object.keys(req.query).length < 2 || !req.query?.username) {
         res
          .status(400)
          .send("Not enough query parameters. Check documentation.");
+        console.log("/updateUser - 400 - Bad request");
         return;
     }
     
@@ -132,10 +131,9 @@ userRouter.put("/updateUser", function (req, res) {
 
     if (!req?.session?.profile || req.session.profile.username !== username) {
         res.status(403).send("Not authorized");
+        console.log("/updateUser - 403 - Not authorized");
         return;
     }
-
-    console.log(`Update request for ${username} recieved`);
 
     for (let i = 0; i < database.users.length; i++) {
         if (database.users[i].username === req.query.username) {
@@ -150,11 +148,13 @@ userRouter.put("/updateUser", function (req, res) {
                 isAdmin: user.isAdmin,
                 bio: user.bio
             });
+            console.log(`/updateUser - 200 - ${user.username}`);
             return;
         }
     }
 
     res.status(404).send("Not found");
+    console.log(`/updateUser - 404 - ${req.session.profile.username}`);
 });
 
 userRouter.delete("/deleteUser", function (req, res) {
@@ -162,6 +162,7 @@ userRouter.delete("/deleteUser", function (req, res) {
 
     if (!user || !user.username) {
         res.status(400).send("No 'username' query parameter");
+        console.log("/deleteUser - 400 - Bad request");
         return;
     }
 
@@ -169,30 +170,33 @@ userRouter.delete("/deleteUser", function (req, res) {
     //  or they're not authorized to delete this account
     if (!req.session || !req?.session?.profile || (!req.session.profile.isAdmin && req.session.profile.username !== user.username)) {
         res.status(403).send("Not authorized");
+        console.log("/deleteUser - 403 - Not authorized");
         return;
     }
-
-    console.log(`Deletion request for ${user.username} recieved`);
 
     for (let i = 0; i < database.users.length; i++) {
         if (database.users[i].username === req.query.username) {
             database.users.splice(i,1);
             res.sendStatus(200);
+            console.log(`/deleteUser - 200 - ${user.username}`);
             return;
         }
     }
 
     res.status(404).send("Not found");
+    console.log(`/updateuser - 404 - ${user.username}`);
 });
 
 gamejamRouter.post("/postJam", function (req, res) {
     if (!req.query.title || !req.query.date || !req.query.description) {
         res.status(400).send("No 'title', 'date', or 'description' query parameters.");
+        console.log("/postJam - 400 - Bad request");
         return;
     }
 
     if (!req.session || !req?.session?.profile || !req.session.profile.isAdmin) {
         res.status(403).send("Not authorized");
+        console.log("/postJam - 403 - Not authorized");
         return;
     }
 
@@ -200,6 +204,7 @@ gamejamRouter.post("/postJam", function (req, res) {
         const jam = database.gamejams[i];
         if (jam.title === req.query.title) {
             res.status(400).send("Title taken");
+            console.log("/postJam - 400 - Title taken");
             return;
         }
     }
@@ -209,56 +214,50 @@ gamejamRouter.post("/postJam", function (req, res) {
         participants: [],
         posts: []
     };
-    
-    console.log(`Request to create game jam '${jam.title}' recieved`);
 
     database.gamejams.push(jam);
     
     res
      .status(201)
      .send(jam);
+    console.log(`/postJam - 201 - ${jam.title}`);
 });
 
 gamejamRouter.get("/getJam", function (req, res) {
     if (!req.query.title) {
         res.status(400).send("No 'title' query parameter");
+        console.log("/getJam - 400 - Bad request");
         return;
     }
-    
-    let found = false;
-
-    console.log(`Request for game jam '${req.query.title}' recieved`);
 
     for (let i = 0; i < database.gamejams.length; i++) {
-        console.log(database.gamejams[i]);
         if (database.gamejams[i].title === req.query.title) {
-            found = true;
             res.status(200).send(database.gamejams[i]);
+            console.log(`/getJam - 200 - ${database.gamejams[i].title}`);
             return;
         }
     }
 
-    if (!found) {
-        console.log("404 error");
-        res.status(404).send("Not found");
-    }
+    res.status(404).send("Not found");
+    console.log(`/getJam - 404 - ${req.query.title}`);
 });
 
 gamejamRouter.get("/getJams", function (req, res) {
-    console.log(`Request for all game jams recieved`);
-
     if (database.gamejams.length === 0) {
         res.status(404).send([]);
+        console.log("/getJams - 404 - No jams");
     } else {
         res.status(200).send(database.gamejams);
+        console.log(`/getJams - 200`);
     }
 });
 
 gamejamRouter.put("/updateJam", function (req, res) {
-    if (Object.keys(req.query).length < 2) {
+    if (Object.keys(req.query).length < 2 || !req.query?.title) {
         res
          .status(400)
          .send("Not enough query parameters. Check documentation.");
+        console.log("/updateJam - 400 - Bad request");
         return;
     }
     
@@ -266,10 +265,9 @@ gamejamRouter.put("/updateJam", function (req, res) {
 
     if (!req?.session?.profile || !req.session.profile.isAdmin) {
         res.status(403).send("Not authorized");
+        console.log("/updateJam - 403 - Not authorized");
         return;
     }
-
-    console.log(`Update game jam request for ${title} recieved`);
 
     for (let i = 0; i < database.gamejams.length; i++) {
         if (database.gamejams[i].title === title) {
@@ -280,21 +278,25 @@ gamejamRouter.put("/updateJam", function (req, res) {
             jam.date = req.query.date ? req.query.date : jam.date;
 
             res.status(200).send(jam);
+            console.log(`/updateJam - 200 - ${jam.title}`);
             return;
         }
     }
 
     res.status(404).send("Not found");
+    console.log(`/updateJam - 404 - ${req.query.title}`);
 });
 
 gamejamRouter.put("/addOrRemoveParticipant", function (req, res) {
     if (!req.query.title) {
         res.status(400).send("No 'title' query parameter");
+        console.log("/addOrRemoveParticipant - 400 - Bad request");
         return;
     }
 
     if (!req?.session?.profile?.username) {
         res.status(401).send("Not logged in");
+        console.log("/addOrRemoveParticipant - 400 - Not logged in");
         return;
     }
 
@@ -310,6 +312,7 @@ gamejamRouter.put("/addOrRemoveParticipant", function (req, res) {
     if (jam === null) {
         console.log("Request to add participant to non-existant game jam received");
         res.status(404).send("Not found");
+        console.log(`/addOrRemoveParticipant - 404 - ${req.query.title}`);
         return;
     }
 
@@ -318,14 +321,14 @@ gamejamRouter.put("/addOrRemoveParticipant", function (req, res) {
         if (participants[i] === newParticipant) {
             jam.participants.splice(i,1);
             res.status(200).send(jam);
-            console.log(`Request to remove ${req.session.profile.username} from '${jam.title}' participants received`);
+            console.log(`/addOrRemoveParticipant - 200 - Remove ${req.session.profile.username}`);
             return;
         }
     }
 
     jam.participants.push(newParticipant);
     res.status(200).send(jam);
-    console.log(`Request to add ${req.session.profile.username} to '${jam.title}' participants received`);
+    console.log(`/addOrRemoveParticipant - 200 - Add ${req.session.profile.username}`);
 });
 
 gamejamRouter.delete("/deleteJam", function (req, res) {
@@ -333,6 +336,7 @@ gamejamRouter.delete("/deleteJam", function (req, res) {
 
     if (!jam || !jam.title) {
         res.status(400).send("No 'title' query parameter");
+        console.log("/deletJam - 400 - Bad request");
         return;
     }
 
@@ -340,20 +344,21 @@ gamejamRouter.delete("/deleteJam", function (req, res) {
     //  or they're not authorized to delete game jams
     if (!req?.session?.profile || !req.session.profile.isAdmin) {
         res.status(403).send("Not authorized");
+        console.log("/deletJam - 403 - Not authorized");
         return;
     }
-
-    console.log(`Deletion request for game jam '${jam.title}' recieved`);
 
     for (let i = 0; i < database.gamejams.length; i++) {
         if (database.gamejams[i].title === jam.title) {
             database.gamejams.splice(i,1);
             res.sendStatus(200);
+            console.log(`/deletJam - 200 - ${req.query.title}`);
             return;
         }
     }
 
     res.status(404).send("Not found");
+    console.log(`/deletJam - 404 - ${req.query.title}`);
 });
 
 app.listen(PORT);
