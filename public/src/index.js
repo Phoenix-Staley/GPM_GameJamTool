c3624999fdff133d122dbab70645d2fe7e338283
martinhundrup <hundrupm@gmail.com>
2024-04-23 19:59:44 -0700

    started on game jam stuff.

--- public/src/index.js
+++ public/src/index.js
@@ -2,5 +2,43 @@
 
 // called when the page is loaded
 document.addEventListener('DOMContentLoaded', function() {
+  createJamTest();
+  set_all_game_jams();
+});
 
-  });
+// initializations
+let all_game_jams = [];
+
+async function createJamTest(){
+  const response = await fetch(
+    'http://localhost:3000/postJam?' + new URLSearchParams({
+      titel: 'Test Jam',
+      date: 'April 23',
+      description: 'test description'
+    }), 
+    {method: 'POST'}
+    );
+
+    let status = response.status;
+    console.log('inside test' + status);
+}
+
+// sets the array of gamejams
+async function set_all_game_jams(){
+
+  const response = await fetch(
+  'http://localhost:3000/getJams?', 
+  {method: 'POST'}
+  );
+  
+  let status = response.status; // 201 if successfull, 404 if no jams
+
+  if (status === 404){ // no jams found
+  }
+  else if (status === 201){ // success
+    all_game_jams = response.json(); // not sure if correct format
+  }
+  else{ // idk something got fucked up
+
+  }
+}
