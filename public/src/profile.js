933368928009a9bf84017aa0a444a58da75eb4ff
martinhundrup <hundrupm@gmail.com>
2024-04-24 21:04:43 -0700

    updates username and bio on profile page.

--- public/src/profile.js
+++ public/src/profile.js
@@ -1,3 +1,8 @@
+// called when the page is loaded
+document.addEventListener('DOMContentLoaded', function() {
+    update_page(test_user);
+});
+
 /*
 profile object
 {
@@ -7,3 +12,21 @@ profile object
     bio: "string"
 }
 */
+
+// inits
+const profile_title = document.getElementById('profile_title');
+const bio = document.getElementById('bio');
+
+
+// testing
+const test_user = {
+  username: 'test user',
+  name: 'first last',
+  isAdmin: false,
+  bio: 'test user bio here'
+}
+
+function update_page(user){
+  profile_title.textContent = user.username;
+  bio.textContent = user.bio;
+}
